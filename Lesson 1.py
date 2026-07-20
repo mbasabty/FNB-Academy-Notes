d87ab961780af833ba580ca17ade58c59ced357f
Mbasa <mbasabatyi@icloud.com>
2026-07-20 21:30:41 +0200

    unit 1

---
+++ Lesson 1.py
@@ -0,0 +1,10 @@
+# Making the computer speak
+print("Hello, World!")
+
+# storing a value in a variable
+name = "Mbasa"
+print(name)
+
+# making our code interactive
+name = input("What is your name? ")
+print(f"Greetings, {name}!")
